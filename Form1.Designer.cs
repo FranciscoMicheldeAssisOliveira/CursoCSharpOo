1a6bb382157baa3063669b591ada04a56c252bcb
FranciscoMicheldeAssisOliveira <michel22assis@gmail.com>
2023-03-02 15:53:56 -0300

    Capitulo 3: Solução até o exercício 3

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,33 +29,96 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.Location = new System.Drawing.Point(247, 125);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(163, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Calcular Média das Idades";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(140, 135);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "1 - Exercício";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(140, 164);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "2 - Exercicio";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(247, 154);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Solução";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(140, 194);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "3 - Exercício";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(247, 184);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Solução";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -19,7 +19,28 @@ namespace Bannco
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Cruso de C# da Caelum");
+            int idadeJoao = 10;
+            int idadeCarlos = 20;
+            int idadeAlisson = 30;
+
+
+            int media = (idadeAlisson + idadeCarlos + idadeJoao) / 3;
+
+            MessageBox.Show("Média das idades: " + media);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // int pi = 3.14;
+            MessageBox.Show("O número 3.14 e do tipo Flooat, e int espera o numero inteiro. " +
+                "O código não compila, pois 3.14 não cabe dentro de um inteiro");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            double pi = 3.14;
+            int piQuebrado = (int) pi;
+            MessageBox.Show("piQuebrado = " + piQuebrado);
         }
     }
 }
